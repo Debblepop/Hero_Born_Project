07c01f9be2590a3f4a3982198948a2403cdb7cf9
Debblepop <158493592+Debblepop@users.noreply.github.com>
2024-04-30 09:40:29 -0400

    Added a rotation speed script

--- Hero_Born_UnityProject/Assets/Scripts/ItemRoatation.cs
+++ Hero_Born_UnityProject/Assets/Scripts/ItemRoatation.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class ItemRoatation : MonoBehaviour
 {
+    public int RotationSpeed = 100;
+    private Transform itemTransform;
     // Start is called before the first frame update
     void Start()
     {
-        
+        itemTransform = this.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        itemTransform.Rotate(RotationSpeed * Time.deltaTime, 0, 0);
     }
 }
