ca5c566f7a57b54528d0a3bd7c9e1a52824769af
Debblepop <158493592+Debblepop@users.noreply.github.com>
2024-04-30 22:04:51 -0400

    Update PlayerBehavoir.cs

--- Hero_Born_UnityProject/Assets/Scripts/PlayerBehavoir.cs
+++ Hero_Born_UnityProject/Assets/Scripts/PlayerBehavoir.cs
@@ -9,10 +9,12 @@ public class PlayerBehavoir : MonoBehaviour
 
     private float _vInput;
     private float _hInput;
+
+    private Rigidbody _rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,7 +23,19 @@ public class PlayerBehavoir : MonoBehaviour
         _vInput = Input.GetAxis("Vertical") * MoveSpeed;
         _hInput = Input.GetAxis("Horizontal") * RotateSpeed;
 
+        /*
         this.transform.Translate(Vector3.forward * _vInput * Time.deltaTime);
         this.transform.Rotate(Vector3.up * _hInput * Time.deltaTime);
+        */
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 rotation = Vector3.up * _hInput;
+
+        Quaternion angleRot = Quaternion.Euler(rotation * Time.fixedDeltaTime);
+
+        _rb.MovePosition(this.transform.position + this.transform.forward * _vInput * Time.fixedDeltaTime);
+        _rb.MoveRotation(_rb.rotation * angleRot);
     }
 }
